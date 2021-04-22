f1fe95a563feadb1826c3743ceccb7d30e64626d
v_shunlzhu <284735415@qq.com>
2021-04-22 20:03:26 +0800

    :emoji1: :emoji2: 提交信息主体

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -14,14 +14,6 @@
         <p class="line ">晓看红湿处, 花重锦官城</p>
       </div>
 
-      <!-- <div class="introduce">
-        <h1 class="title">春夜喜雨</h1>
-        <p class="line ">朝代:唐朝 | 作者:杜甫</p>
-        <p class="line ">好雨知时节, 当春乃发生</p>
-        <p class="line ">随风潜入夜, 润物细无声</p>
-        <p class="line ">野径云俱黑, 江船火独明</p>
-        <p class="line ">晓看红湿处, 花重锦官城</p>
-      </div>
       <div class="introduce">
         <h1 class="title">春夜喜雨</h1>
         <p class="line ">朝代:唐朝 | 作者:杜甫</p>
@@ -37,7 +29,15 @@
         <p class="line ">随风潜入夜, 润物细无声</p>
         <p class="line ">野径云俱黑, 江船火独明</p>
         <p class="line ">晓看红湿处, 花重锦官城</p>
-      </div> -->
+      </div>
+      <div class="introduce">
+        <h1 class="title">春夜喜雨</h1>
+        <p class="line ">朝代:唐朝 | 作者:杜甫</p>
+        <p class="line ">好雨知时节, 当春乃发生</p>
+        <p class="line ">随风潜入夜, 润物细无声</p>
+        <p class="line ">野径云俱黑, 江船火独明</p>
+        <p class="line ">晓看红湿处, 花重锦官城</p>
+      </div>
     </div>
   </div>
 </template>
